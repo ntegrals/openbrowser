890d805b9cd4d252b0ed05ec84f71d7857955417
Julian Schoen <j.schoen@mail.com>
2019-09-03 16:12:49 -0500

    export commands and errors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,64 @@
+// open-browser - Programmable browser automation toolkit
+
 export { Viewport } from './viewport';
-export { EventHub } from './event-hub';
+export type { ViewportEvents } from './viewport';
+
 export { DomInspector } from './dom-inspector';
+
+export {
+  click,
+  typeText,
+  navigate,
+  scroll,
+  screenshot,
+  goBack,
+  goForward,
+  pressKey,
+  hover,
+  selectOption,
+  waitForNavigation,
+} from './commands';
+
+export { EventHub } from './event-hub';
+
+export {
+  OpenBrowserError,
+  ViewportError,
+  LaunchFailedError,
+  NavigationFailedError,
+  CommandError,
+  ElementNotFoundError,
+  TimeoutError,
+} from './errors';
+
+export {
+  createConfig,
+  validateConfig,
+  DEFAULT_CONFIG,
+} from './config';
+export type { ViewportConfig } from './config';
+
+export { createLogger, setLogLevel, LogLevel } from './logging';
+
+export {
+  generateId,
+  sleep,
+  sanitizeText,
+  truncateText,
+  stripTags,
+  withTimeout,
+  isValidUrl,
+  normalizeUrl,
+} from './utils';
+
+export type {
+  ElementRef,
+  Position,
+  Rect,
+  Result,
+  ViewportSize,
+  ElementInfo,
+  CommandResult,
+  ScreenshotData,
+  PageInfo,
+} from './types';
