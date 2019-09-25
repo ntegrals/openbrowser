924c10e12e76f91937bfd1dec848a60a767be8e7
Julian Schoen <j.schoen@mail.com>
2019-09-24 19:26:59 -0500

    finish command tests

--- src/__tests__/commands.test.ts
+++ src/__tests__/commands.test.ts
@@ -78,3 +78,111 @@ describe('commands', () => {
   describe('navigate', () => {
     it('should navigate to a URL', async () => {
       const result = await commands.navigate(page, 'https://example.com');
+      expect(result.success).toBe(true);
+      expect(page.goto).toHaveBeenCalledWith('https://example.com', {
+        waitUntil: 'domcontentloaded',
+        timeout: 30000,
+      });
+    });
+
+    it('should auto-prepend https://', async () => {
+      await commands.navigate(page, 'example.com');
+      expect(page.goto).toHaveBeenCalledWith(
+        'https://example.com',
+        expect.any(Object),
+      );
+    });
+
+    it('should not modify about: URLs', async () => {
+      await commands.navigate(page, 'about:blank');
+      expect(page.goto).toHaveBeenCalledWith(
+        'about:blank',
+        expect.any(Object),
+      );
+    });
+  });
+
+  describe('scroll', () => {
+    it('should scroll down', async () => {
+      const result = await commands.scroll(page, 'down', 500);
+      expect(result.success).toBe(true);
+      expect(page.evaluate).toHaveBeenCalled();
+    });
+
+    it('should scroll up', async () => {
+      const result = await commands.scroll(page, 'up', 300);
+      expect(result.success).toBe(true);
+    });
+
+    it('should use default amount of 300px', async () => {
+      await commands.scroll(page, 'down');
+      const evalCall = page.evaluate.mock.calls[0];
+      // Second argument is the scroll amount
+      expect(evalCall[2]).toBe(300); // y amount
+    });
+  });
+
+  describe('screenshot', () => {
+    it('should take a screenshot', async () => {
+      const result = await commands.screenshot(page);
+      expect(result.buffer).toBeDefined();
+      expect(result.width).toBe(1280);
+      expect(result.height).toBe(800);
+    });
+
+    it('should support fullPage option', async () => {
+      await commands.screenshot(page, { fullPage: true });
+      expect(page.screenshot).toHaveBeenCalledWith(
+        expect.objectContaining({ fullPage: true }),
+      );
+    });
+  });
+
+  describe('goBack', () => {
+    it('should go back in history', async () => {
+      const result = await commands.goBack(page);
+      expect(result.success).toBe(true);
+      expect(page.goBack).toHaveBeenCalled();
+    });
+  });
+
+  describe('goForward', () => {
+    it('should go forward in history', async () => {
+      const result = await commands.goForward(page);
+      expect(result.success).toBe(true);
+      expect(page.goForward).toHaveBeenCalled();
+    });
+  });
+
+  describe('pressKey', () => {
+    it('should press a key', async () => {
+      const result = await commands.pressKey(page, 'Enter');
+      expect(result.success).toBe(true);
+      expect(page.keyboard.press).toHaveBeenCalledWith('Enter');
+    });
+  });
+
+  describe('hover', () => {
+    it('should hover over an element', async () => {
+      const result = await commands.hover(page, 'div.menu-item');
+      expect(result.success).toBe(true);
+      expect(page.hover).toHaveBeenCalledWith('div.menu-item');
+    });
+  });
+
+  describe('selectOption', () => {
+    it('should select an option', async () => {
+      const result = await commands.selectOption(page, 'select#country', 'US');
+      expect(result.success).toBe(true);
+      expect(page.select).toHaveBeenCalledWith('select#country', 'US');
+    });
+  });
+
+  describe('waitForNavigation', () => {
+    it('should wait for navigation', async () => {
+      const result = await commands.waitForNavigation(page);
+      expect(result.success).toBe(true);
+      expect(page.waitForNavigation).toHaveBeenCalled();
+    });
+  });
+});
