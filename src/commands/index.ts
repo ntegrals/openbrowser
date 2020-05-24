7d38d34eb1837da1d58dcf71f428ee168857b7ee
Julian Schoen <j.schoen@mail.com>
2020-05-24 12:31:07 -0500

    add commands barrel export

---
+++ src/commands/index.ts
@@ -0,0 +1,5 @@
+export { click } from './click';
+export { typeText } from './type';
+export { navigate } from './navigate';
+export { scroll } from './scroll';
+export { screenshot } from './screenshot';
