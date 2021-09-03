28413c6dd74c555b0f146d8d54b41c6254c4642a
Julian Schoen <j.schoen@mail.com>
2021-09-03 09:50:49 -0500

    add page types

---
+++ src/page/index.ts
@@ -0,0 +1,4 @@
+export { renderTree } from './renderer/tree-renderer';
+export type { TreeNode } from './renderer/tree-renderer';
+export { isInteractive } from './renderer/interactive-elements';
+export type { DomNode, PageSnapshot, ScrollPosition } from './types';
