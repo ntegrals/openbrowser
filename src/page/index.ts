4c9c3d29469e112bd260215e1a5b1f67aafca142
Julian Schoen <j.schoen@mail.com>
2022-03-03 12:12:58 -0500

    export snapshot builder

--- src/page/index.ts
+++ src/page/index.ts
@@ -1,4 +1,5 @@
 export { renderTree } from './renderer/tree-renderer';
 export type { TreeNode } from './renderer/tree-renderer';
 export { isInteractive } from './renderer/interactive-elements';
+export { buildSnapshot } from './snapshot-builder';
 export type { DomNode, PageSnapshot, ScrollPosition } from './types';
