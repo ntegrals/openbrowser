1a33b9e6dca2ecfd196321bbb0e30cb2bf1fb9bf
Julian Schoen <j.schoen@mail.com>
2022-11-02 15:09:29 -0500

    export page analyzer

--- src/page/index.ts
+++ src/page/index.ts
@@ -1,5 +1,6 @@
 export { renderTree } from './renderer/tree-renderer';
 export type { TreeNode } from './renderer/tree-renderer';
 export { isInteractive } from './renderer/interactive-elements';
+export { PageAnalyzer } from './page-analyzer';
 export { buildSnapshot } from './snapshot-builder';
 export type { DomNode, PageSnapshot, ScrollPosition } from './types';
