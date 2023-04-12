dffb2dc65e505a3740f57b4862179f83b6bff243
Julian Schoen <j.schoen@mail.com>
2023-04-12 16:44:02 -0500

    export model adapters from index

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,2 +1,3 @@
 export type { LanguageModel } from './interface';
+export { OpenAIRawAdapter } from './adapters/openai-raw';
 export type { ModelMessage, ModelResponse, ModelOptions, ToolDefinition, ToolCall } from './types';
