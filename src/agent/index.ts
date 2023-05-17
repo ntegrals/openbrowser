e46b984d4e0c2aa8991e3c6ebd5eea94d7b247fe
Julian Schoen <j.schoen@mail.com>
2023-05-16 21:38:28 -0500

    add agent module exports

---
+++ src/agent/index.ts
@@ -0,0 +1,5 @@
+export { Agent } from './agent';
+export { ConversationManager } from './conversation/service';
+export { StallDetector } from './stall-detector';
+export { InstructionBuilder } from './instructions';
+export type { AgentStep, AgentResult, AgentOptions } from './types';
