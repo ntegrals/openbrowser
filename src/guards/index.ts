02126b31b62352487133b87a3424c7a97d0e81df
Julian Schoen <j.schoen@mail.com>
2024-05-13 20:33:24 -0500

    cleanup: remove old guards directory

--- src/guards/index.ts
+++
@@ -1,7 +0,0 @@
-export { BaseGuard } from './base-guard';
-export type { GuardContext } from './base-guard';
-export { BlankPageGuard } from './blank-page';
-export { CrashGuard } from './crash';
-export { PopupGuard } from './popups';
-export { UrlPolicyGuard } from './url-policy';
-export { PageReadyGuard } from './page-ready';
