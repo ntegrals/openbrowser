80f8d03fff5926d0d77eebe0f1c8d8f39870790a
Julian Schoen <j.schoen@mail.com>
2024-06-03 09:22:23 -0500

    feat: add composite usage meter

--- src/metering/index.ts
+++ src/metering/index.ts
@@ -1,2 +1,5 @@
 export { UsageMeter } from './tracker';
 export type { UsageRecord, UsageSnapshot } from './types';
+
+// Re-export as CompositeUsageMeter for backwards compat
+export { UsageMeter as CompositeUsageMeter } from './tracker';
