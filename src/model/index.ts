ea2261e72c3794ed3960097a723d893f31370856
Julian Schoen <j.schoen@mail.com>
2024-10-18 16:08:32 -0500

    export vercel adapter from model index

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,3 +1,4 @@
 export type { LanguageModel } from './interface';
+export { VercelModelAdapter } from './adapters/vercel';
 export { OpenAIRawAdapter } from './adapters/openai-raw';
 export type { ModelMessage, ModelResponse, ModelOptions, ToolDefinition, ToolCall } from './types';
