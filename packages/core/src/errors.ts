d985d486194b11fbb455053794ab0b733d2d9d30
Julian Schoen <j.schoen@mail.com>
2025-01-08 09:24:00 -0500

    continue errors implementation

--- packages/core/src/errors.ts
+++ packages/core/src/errors.ts
@@ -48,3 +48,53 @@ export class AgentStalledError extends AgentError {
 	constructor(message = 'Agent is stuck in a loop', options?: ErrorOptions) {
 		super(message, options);
 		this.name = 'AgentStalledError';
+	}
+}
+
+export class StepLimitExceededError extends AgentError {
+	public readonly stepsTaken: number;
+	public readonly stepLimit: number;
+
+	constructor(stepsTaken: number, stepLimit: number, options?: ErrorOptions) {
+		super(`Agent reached maximum steps (${stepsTaken}/${stepLimit})`, options);
+		this.name = 'StepLimitExceededError';
+		this.stepsTaken = stepsTaken;
+		this.stepLimit = stepLimit;
+	}
+}
+
+export class UrlBlockedError extends OpenBrowserError {
+	public readonly url: string;
+
+	constructor(url: string, options?: ErrorOptions) {
+		super(`URL not allowed: ${url}`, options);
+		this.name = 'UrlBlockedError';
+		this.url = url;
+	}
+}
+
+export class PageExtractionError extends OpenBrowserError {
+	constructor(message: string, options?: ErrorOptions) {
+		super(message, options);
+		this.name = 'PageExtractionError';
+	}
+}
+
+export class ModelError extends OpenBrowserError {
+	constructor(message: string, options?: ErrorOptions) {
+		super(message, options);
+		this.name = 'ModelError';
+	}
+}
+
+export class ModelThrottledError extends ModelError {
+	public readonly retryAfterMs?: number;
+
+	constructor(message: string, retryAfterMs?: number, options?: ErrorOptions) {
+		super(message, options);
+		this.name = 'ModelThrottledError';
+		this.retryAfterMs = retryAfterMs;
+	}
+}
+
+export class CommandFailedError extends OpenBrowserError {
