d226e7a04c6ef9c35ec84625182fb5a33abde936
Julian Schoen <j.schoen@mail.com>
2025-01-17 12:24:56 -0500

    update viewport exports

---
+++ packages/core/src/viewport/index.ts
@@ -0,0 +1,26 @@
+export { Viewport, type ViewportOptions } from './viewport.js';
+export { LaunchProfile } from './launch-profile.js';
+export { EventHub } from './event-hub.js';
+export { BaseGuard, type GuardContext } from './guard-base.js';
+export { VisualTracer, type VisualTracerOptions } from './visual-tracer.js';
+export {
+	type TabDescriptor,
+	type ViewportSnapshot,
+	type ViewportHistory,
+	type LaunchOptions,
+	type PageState,
+} from './types.js';
+export {
+	type ViewportEventMap,
+	type ViewportRequestMap,
+	type NavigateEvent,
+	type ClickEvent,
+	type InputEvent,
+	type ScrollEvent,
+	type ScreenshotEvent,
+	type ScreenshotResult,
+	type DownloadEvent,
+	type PopupEvent,
+	type SecurityEvent,
+	type CrashEvent,
+} from './events.js';
