507ca887077c78e492fdec5bc9b792c31719a8ed
Julian Schoen <j.schoen@mail.com>
2025-01-24 12:24:06 -0500

    update bridge exports

---
+++ packages/core/src/bridge/index.ts
@@ -0,0 +1,3 @@
+export { BridgeServer, type BridgeServerOptions } from './server.js';
+export { BridgeClient, type BridgeClientOptions } from './client.js';
+export { BridgeAdapter } from './adapter.js';
