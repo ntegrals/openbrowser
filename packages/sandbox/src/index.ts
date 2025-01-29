f9ff7d194c27a88dc3dcce400cdd2fcf5114275d
Julian Schoen <j.schoen@mail.com>
2025-01-29 11:02:04 -0500

    feat(sandbox): add index

---
+++ packages/sandbox/src/index.ts
@@ -0,0 +1,10 @@
+export { Sandbox } from './sandbox.js';
+export type {
+	SandboxOptions,
+	SandboxResult,
+	SandboxError,
+	SandboxErrorCategory,
+	SandboxMetrics,
+	CapturedOutput,
+	ResourceSnapshot,
+} from './types.js';
